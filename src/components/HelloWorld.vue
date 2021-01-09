bce8633ab72ed6f1484289693afd65e5c7d8b9aa
wenyajun2011 <153165057@qq.com>
2021-01-09 20:22:26 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="hello">
     <div class="block">
-  <span class="demonstration">默认 click 触发子菜单12</span>
-  <el-cascader
+
+  <!-- <span class="demonstration">默认 click 触发子菜单12</span> -->
+  <!-- <el-cascader
     v-model="value"
     :options="options"
-    @change="handleChange"></el-cascader>
+    @change="handleChange"></el-cascader> -->
+    
+
+    <!-- <div class="imageFrame">
+    <input name="upimage" id="upimage" type="file">
+    <input type="button" id="upimageButton" onclick="upimage.click()">
+    <img id="showimage" src="" alt="">
+    </div> -->
+
 </div>
   </div>
 </template>
 
 <script>
+import '../../public/exif.min';
+import compressImg from '../../public/processImg'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+   mounted () {
+      compressImg('upimage','showimage',480,function(src){
+        //此处为回调函数，当图片压缩完成并成功显示后执行
+        //可得到图片数据值src
+        console.log(src);
+  });
+   },
+  created () {
+    
   }
 }
 </script>
